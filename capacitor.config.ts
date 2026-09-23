fa436bc553f091159fbdb4a82f21b24345eca71f
lzd4442 <lzd422@zohomail.cn>
2026-09-23 21:54:21 +0800

    add capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.totp.app',
+  appName: 'TOTPApp',
+  webDir: 'dist'
+};
+
+export default config;
